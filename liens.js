94c4b180d6752b5f457ce03a06a18b72b23c2436
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-03-15 23:05:19 +0100

    Update liens.js

--- liens.js
+++ liens.js
@@ -15,17 +15,3 @@ exemple = {
 	"Royaume de Suède": {},
 	"Royaume de Jérusalem": {},
 }
-
-/* ** = {
-	"Empire français": {
-		"Bonne ville": [
-			Wikipédia + "d/df/Ornements_extérieurs_Bonne_ville_du_Premier_Empire.svg",
-		],
-		"Ville de 2de classe": [
-			Wikipédia + "2/2b/Orn_ext_ville_d'empire_2nde_class.svg",
-		],
-		"Ville de 3e classe": [
-			Wikipédia + "0/05/Ornements_extérieurs_ville_de_3e_classe_de_l'Empire_français.svg",
-		],
-	},
-} */
