4488f06c3c10d003a26cffdbdacb599cf8b47352
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-03-17 22:52:40 +0100

    Update heaumes.js

--- liens/heaumes.js
+++ liens/heaumes.js
@@ -38,8 +38,6 @@ heaumes = {
 			"armoirie/heaumes/RF Gentilhomme d'ancienne race-A.png",
 			"armoirie/heaumes/RF Gentilhomme d'ancienne race-B.png",
 		],
-		// "Banneret": [],
-		// "Chevalier": [],
 		"Gentilhomme de trois races": [
 			"armoirie/heaumes/RF Gentilhomme de trois races-A.png",
 			"armoirie/heaumes/RF Gentilhomme de trois races-B.png",
@@ -53,8 +51,6 @@ heaumes = {
 			"armoirie/heaumes/RF Bâtard-B.png",
 		],
 	},
-	// "Empire français": {},
-	// "Royaume d'Espagne": {},
 	"Royaume-Uni": {
 		"Roi": [
 			Wikipédia + "5/52/Royal_Coat_of_Arms_of_the_United_Kingdom-Helmet.svg",
@@ -63,19 +59,14 @@ heaumes = {
 		"Pair": [
 			Wikipédia + "c/c8/Peer_helmet_in_heraldry.svg",
 		],
-		"Baronnet": [ // ou chevalier
+		"Baronnet": [
 			Wikipédia + "e/e9/Héraldique_ornement_Heaume_baronnet.svg",
 			Wikipédia + "8/83/Helm_of_a_Baronet_or_Knight_in_British_Heraldry.svg",
 		],
-		"Gentilhomme": [ // ou écuyer
+		"Gentilhomme": [
 			Wikipédia + "1/1e/Helm_of_an_Esquire_or_Gentleman_in_British_Heraldry.svg",
 		],
 	},
-	// "Saint-Empire romain germanique": {},
-	// "État du Vatican": {},
-	// "Royaume du Portugal": {},
-	// "Grand-Duché de Luxembourg": {},
-	// "Royaume de Suède": {},
 	"Royaume de Jérusalem": {
 		"Roi": [
 			Wikipédia + "5/52/Royal_Coat_of_Arms_of_the_United_Kingdom-Helmet.svg",
